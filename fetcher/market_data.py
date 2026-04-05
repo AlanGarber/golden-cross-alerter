81a094732065061bc1df3e914e420053bd835079
Alan Garber <alangarber1@gmail.com>
2026-04-05 01:44:39 -0300

    fix: use StringIO to parse Wikipedia HTML correctly

--- fetcher/market_data.py
+++ fetcher/market_data.py
@@ -16,8 +16,9 @@ def get_historical_data(symbol: str, period: str = "2y") -> pd.DataFrame:
 
 def get_sp500_symbols() -> list[str]:
     import requests
+    from io import StringIO
     headers = {"User-Agent": "Mozilla/5.0"}
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     response = requests.get(url, headers=headers)
-    df = pd.read_html(response.text)[0]
+    df = pd.read_html(StringIO(response.text))[0]
     return df["Symbol"].tolist()
